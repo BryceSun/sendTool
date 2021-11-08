e0d2033f9c8b63cf0d1a99ca438529c8ed2260fe
bryce <qingyunxi@foxmail.com>
2021-11-08 18:35:01 +0800

    fix

--- main.go
+++ main.go
@@ -12,43 +12,43 @@ import (
 )
 
 type collection struct {
-	Host string `json:"host"`
-	Name string `json:"collectionName"`
-	Requestes []rq `json:"requestes"`
+	Host      string `json:"host"`
+	Name      string `json:"collectionName"`
+	Requestes []rq   `json:"requestes"`
 }
 type rq struct {
-	Name string `json:"name"`
-	Path string `json:"path"`
+	Name   string `json:"name"`
+	Path   string `json:"path"`
 	Method string `json:"method"`
 	Params string `json:"params"`
 }
 
 func main() {
-	host := flag.String("host","http://localhost","请求地址的域名")
-	file := flag.String("file","./ioc_requestes.json","存放请求的json文件")
+	host := flag.String("host", "http://localhost", "请求地址的域名")
+	file := flag.String("file", "./ioc_requestes.json", "存放请求的json文件")
 
 	flag.Parse()
-	fmt.Println("host:",*host)
-	fmt.Println("file",*file)
-	bs,err := ioutil.ReadFile(*file)
+	fmt.Println("host:", *host)
+	fmt.Println("file", *file)
+	bs, err := ioutil.ReadFile(*file)
 	if err != nil {
 		log.Println(err)
 	}
 	//fmt.Println(string(bs))
 	c := new(collection)
-	err = json.Unmarshal(bs,c)
+	err = json.Unmarshal(bs, c)
 	if err != nil {
 		log.Println(err)
 	}
-	if len(*host) == 0 {
+	if len(c.Host) != 0 {
 		*host = c.Host
 	}
-	for _,q := range c.Requestes {
-		req, err := http.NewRequest("POST", strings.ReplaceAll(q.Path,"#host",*host), bytes.NewBufferString(q.Params))
+	for _, q := range c.Requestes {
+		req, err := http.NewRequest("POST", strings.ReplaceAll(q.Path, "#host", *host), bytes.NewBufferString(q.Params))
 		if err != nil {
 			log.Println(err)
 		}
-		req.Header.Set("Content-Type","application/json")
+		req.Header.Set("Content-Type", "application/json")
 		response, err := http.DefaultClient.Do(req)
 		log.Println(response)
 	}
